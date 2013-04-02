3fa0d3e912bfb00c1ccdf7a2c427a29fd8d9e23f
Piyush Khandelwal <piyushk@gmail.com>
2013-04-02 16:29:34 -0500

    some cleanup in the python script

--- bwi_exp1/scripts/experiment.py
+++ bwi_exp1/scripts/experiment.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 
-# Read yaml file corresponding to graph
-# Read experiment file
-# Publish out text data corresponding to state of the experiment
-# Teleport person and robots around
-# Grab odometry data
-# How do we want the robot to communicate with the human??
-# have a service to aquire a lock onto this experiment
-
 import roslib; roslib.load_manifest('bwi_exp1')
 import rospy
 
@@ -57,7 +49,6 @@ def produceDirectedArrow(up_arrow, yaw):
 
     image = cv2.copyMakeBorder(resized_image, top, bottom, left, right, cv2.BORDER_CONSTANT, image, (0,0,0))
     return image
-  
 
 class RobotScreenPublisher(threading.Thread):
 
@@ -164,18 +155,13 @@ class ExperimentController:
         start_yaw = experiment_data['start_yaw']
         start_pose = getPoseMsgFrom2dData(start_x, start_y, yaw=start_yaw)
         result = self.teleportEntity(self.person_id, start_pose)
-        # print result
 
         # Teleport the goal ball to its correct position
         ball_x = experiment_data['ball_x']
         ball_y = experiment_data['ball_y']
         ball_pose = getPoseMsgFrom2dData(ball_x, ball_y, 0)
         result = self.teleportEntity(self.ball_id, ball_pose)
-        # print result
         self.goal_location = [ball_x, ball_y]
-
-        # compute location/orientation from path?
-        # will require saving the 2 parameters in the graph itself
 
         # Teleport all the robots to their respective positions
         self.path = experiment_data['path']
@@ -184,7 +170,7 @@ class ExperimentController:
         self.robot_locations = [None] * len(self.robots)
         self.robot_images = [None] * len(self.robots)
         for i, robot in enumerate(self.robots):
-            if i < len(self.robots_in_experiment):
+            if i < len(self.robots_in_experiment): #one of the robors being used in this experiment
                 path_position = self.robots_in_experiment[i]['path_position']
 
                 #get position of robot from service
@@ -207,46 +193,27 @@ class ExperimentController:
                     req.to_pt.z = 0
                     going_to = req.to_pt
 
-                #print req
                 resp = self.position_robot(req)
                 robot_loc = [resp.loc.x, resp.loc.y]
                 robot_yaw = resp.yaw
-                #print robot_loc, robot_yaw
-                
+
+                #get orientation of the arrow on the screen
                 destination_yaw = math.atan2(going_to[1] - robot_loc[1], going_to[0] - robot_loc[0])
                 change_in_yaw = destination_yaw - robot_yaw
-                #normalize angle
-                change_in_yaw = math.atan2(math.sin(change_in_yaw), math.cos(change_in_yaw))
+                change_in_yaw = math.atan2(math.sin(change_in_yaw), math.cos(change_in_yaw)) #normalize angle
                 robot_image = produceDirectedArrow(self.arrow, change_in_yaw)
-                # if change_in_yaw > 0.75 * math.pi or change_in_yaw <= -0.75 * math.pi:
-                #     robot_image = self.arrow_down
-                # elif change_in_yaw > 0.25 * math.pi and change_in_yaw <= 0.75 * math.pi:
-                #     robot_image = self.arrow_left
-                # elif change_in_yaw > -0.25 * math.pi and change_in_yaw <= 0.25 * math.pi:
-                #     robot_image = self.arrow_up
-                # else:
-                #     robot_image = self.arrow_right
-                #move the robot so that it does not block the path anymore
-                # to_pt = [math.cos(destination_yaw), math.sin(destination_yaw)]
-                # from_pt = [math.cos(robot_yaw + math.pi), math.sin(robot_yaw + math.pi)]
-                # outer_pt = [-(qi + qj)/2.0 for qi, qj in zip(to_pt, from_pt)]
-                # print to_pt, from_pt, outer_pt
-                # if max([math.fabs(pt) for pt in outer_pt]) < 0.1: # the 2 vectors are cancelling each other
-                #     move_yaw = destination_yaw + math.pi/2.0
-                # else:
-                #     move_yaw = math.atan2(outer_pt[1], outer_pt[0])
-                # print move_yaw
-                # robot_loc[0] = robot_loc[0] + 0.2 * math.cos(move_yaw)
-                # robot_loc[1] = robot_loc[1] + 0.2 * math.sin(move_yaw)
-                # print robot_loc
-            elif i - len(self.robots_in_experiment) < len(self.extra_robots):
+
+            elif i - len(self.robots_in_experiment) < len(self.extra_robots): #one of the extra robots
                 robot_loc = [self.extra_robots[i - len(self.robots_in_experiment)]['loc_x'], self.extra_robots[i - len(self.robots_in_experiment)]['loc_y']]
                 robot_yaw = self.extra_robots[i - len(self.robots_in_experiment)]['yaw']
                 robot_image = self.image_none
-            else:
+
+            else: #robot not in use in this experiment
                 robot_loc = self.robots[i]['default_loc']
                 robot_yaw = 0
                 robot_image = self.image_none
+
+            #perform the actual teleport and image update
             robot_pose = getPoseMsgFrom2dData(*robot_loc, yaw=robot_yaw)
             result = self.teleportEntity(self.robots[i]['id'], robot_pose)
             self.robot_images[i] = robot_image
@@ -271,7 +238,7 @@ class ExperimentController:
         else:
             self.text_pub.publish("The experiment has timed out. Let's proceed to the next experiment.")
         
-        # TODO Close the odometry write here, plus say if this run was successful 
+        # TODO Close the odometry write here 
 
     def odometryCallback(self, odom):
 
