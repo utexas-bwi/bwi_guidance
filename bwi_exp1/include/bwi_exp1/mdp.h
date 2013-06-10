d5195e763b5b82dfc6224e313f68b3d28a6b7e62
Piyush Khandelwal <piyushk@gmail.com>
2013-06-10 18:09:35 -0500

    switching to the new simpler mdp formulation. will make the formulation more complex at a later stage.

--- bwi_exp1/include/bwi_exp1/mdp.h
+++ bwi_exp1/include/bwi_exp1/mdp.h
@@ -4,33 +4,57 @@
 #include <vector>
 #include <stdint.h>
 
-#define FRONTIER_LOOKAHEAD 3
 #define GAMMA 0.98
 
 namespace bwi_exp1 {
 
-  struct Transition {
-    size_t state;
-    size_t reward;
-    size_t value;
+  static const double GAMMA = 0.98;
+  static const size_t NUM_DIRECTIONS = 16;
+  static const size_t MAX_ROBOTS = 5;
+
+  enum ActionType {
+    DO_NOTHING = 0,
+    PLACE_ROBOT = 1
+  };
+
+  struct Action {
+    ActionType type;
+    size_t robot_id; // used in conjunction with type = PLACE_ROBOT
   };
 
   struct State {
 
-    size_t graph_id;
-    size_t direction; // 0 to 7
-    size_t direction_at_current; // -1 to 7, -1 for no robot
+    size_t graph_id; // ~100
+    size_t direction; // 0 to NUM_DIRECTIONS - 1
+    size_t num_robots_left; // 0 to MAX_ROBOTS
 
-    std::vector<size_t> frontier;
-    size_t extra_visible_robot; // sizeof(frontier) + 1 (for no extra robot)
-    size_t direction_extra_robot;
+    // size_t direction_at_current; // -1 to 7, -1 for no robot
+    // std::vector<size_t> frontier;
+    // size_t extra_visible_robot; // sizeof(frontier) + 1 (for no extra robot)
+    // size_t direction_extra_robot;
 
-    std::vector<Transition> next;
+    std::vector<Action> actions;
+    std::vector<size_t> next_states;
 
   };
 
+  inline size_t getStateSpaceSize(size_t num_vertices) {
+    return num_vertices * NUM_DIRECTIONS * (MAX_ROBOTS + 1);
+  }
+
+  inline size_t constructStateIndex(size_t graph_id, size_t direction, 
+      size_t num_robots_left) {
+
+    return graph_id * NUM_DIRECTIONS * (MAX_ROBOTS + 1) + 
+      direction * (MAX_ROBOTS + 1) +
+      num_robots_left;
+  }
+
   void populateStateSpace(const topological_mapper::Graph &graph, 
-      std::vector<State>& state_space);
+      std::vector<State>& state_space, size_t goal_idx);
+
+  void getTransitionProbabilities(const State& state, const Action& action, 
+      std::vector<double>& p);
 
 }
 
--- bwi_exp1/src/mdp.cc
+++ bwi_exp1/src/mdp.cc
@@ -5,81 +5,109 @@
 
 namespace bwi_exp1 {
 
-  class WeightedVertex {
-   public:
-    size_t vertex;
-    int8_t weight;
-    WeightedVertex(size_t v, int8_t w) : vertex(v), weight(w) {}
-    bool operator==(const WeightedVertex& l, const WeightedVertex& r) {
-      return l.vertex == r.vertex;
-    }
-  };
+  // class WeightedVertex {
+  //  public:
+  //   size_t vertex;
+  //   int8_t weight;
+  //   WeightedVertex(size_t v, int8_t w) : vertex(v), weight(w) {}
+  //   bool operator==(const WeightedVertex& l, const WeightedVertex& r) {
+  //     return l.vertex == r.vertex;
+  //   }
+  // };
 
-  struct weightedVertexComparator : 
-      binary_function <WeightedVertex, WeightedVertex, bool> {
-    inline bool operator() (const WeightedVertex& x, const WeightedVertex& y) const {
-      return x.weight < y.weight;
-    }
-  };
+  // struct weightedVertexComparator : 
+  //     binary_function <WeightedVertex, WeightedVertex, bool> {
+  //   inline bool operator() (const WeightedVertex& x, const WeightedVertex& y) const {
+  //     return x.weight < y.weight;
+  //   }
+  // };
 
-  typedef WeightedVertexHeap boost::heap::fibonacci_heap<
-    WeightedVertex, 
-    boost::heap::compare<weightedVertexComparator>
-  >;
-  typedef topological_mapper::Graph Graph;
+  // typedef WeightedVertexHeap boost::heap::fibonacci_heap<
+  //   WeightedVertex, 
+  //   boost::heap::compare<weightedVertexComparator>
+  // >;
+  // typedef topological_mapper::Graph Graph;
 
-  /* not available publicly */
-  void getVisibleVertices(const topological_mapper::Graph& graph, size_t src,
-      size_t current, std::vector<size_t>& frontier, int8_t depth) {
-    topological_mapper::Graph::vertex_descriptor vd = boost::vertex(src, graph);
+  // void populateStateSpace(const Graph &graph, 
+  //     std::vector<State>& state_space) {
 
-    Graph::adjacency_iterator ai, aend;
-    for (boost::tie(ai, aend) = boost::adjacent_vertices(
-          (Graph::vertex_descriptor)*vi, graph); 
-        ai != aend; ++ai) {
-      
-      Point2f location2 = graph[*ai].location;
-    }
+  //   size_t num_vertices = boost::num_vertices(graph);
+  //   boost::property_map<Graph, boost::vertex_index_t>::type 
+  //       indexmap = boost::get(boost::vertex_index, graph);
 
-  }
+  //   for (size_t i = 0; i < num_vertices; ++i) { //graph_id
 
+  //     Graph::vertex_descriptor source_v = boost::vertex(i, graph);
+  //     topological_mapper::Point2f source_loc = graph[source_v].location;
+
+  //     // Compute frontier at this vertex. 
+  //     State state;
+  //     WeightedVertexHeap heap;
+  //     heap.push(WeightedVertex(i, FRONTIER_LOOKAHEAD));
+
+  //     // Get a visted vector
+  //     std::vector<bool> visited(num_vertices, false); 
+  //     visited[i] = true;
+
+  //     while(!heap.empty()) {
+  //       const WeightedVertex &wv = heap.top();
+  //       
+  //       if (wv.weight == 0) {
+  //         if (wv.vertex != i) // Don't push the source vertex into the 
+  //           state.frontier.push_back(wv.vertex);
+  //       } else {
+
+  //         // Check if any of the adjacent vertices are not already on the heap,
+  //         // and they are still line-of-sight
+  //         bool current_vertex_in_frontier = true;
+  //         Graph::vertex_descriptor v = boost::vertex(wv.vertex, graph);
+  //         Graph::adjacency_iterator ai, aend;
+  //         for (boost::tie(ai, aend) = boost::adjacent_vertices(v, graph); 
+  //             ai != aend; ++ai) {
+
+  //           // Check if adjacent location has already been visited
+  //           if (visited[*ai]) {
+  //             continue;
+  //           }
+
+  //           // Compute location to see if a given place 
+  //           Point2f loc = graph[*ai].location;
+  //         }
+
+
+  //       }
+
+  //       heap.pop();
+  //     }
+
+  //   }
+  // }
+  
   void populateStateSpace(const Graph &graph, 
-      std::vector<State>& state_space) {
+      std::vector<State>& state_space, size_t goal_idx) {
 
     size_t num_vertices = boost::num_vertices(graph);
+    state_space.resize(getStateSpaceSize(num_vertices));
+
     boost::property_map<Graph, boost::vertex_index_t>::type 
         indexmap = boost::get(boost::vertex_index, graph);
-    for (size_t i = 0; i < num_vertices; ++i) { //graph_id
 
-      Graph::vertex_descriptor source_v = boost::vertex(i, graph);
-      topological_mapper::Point2f source_loc = graph[source_v].location;
+    for (size_t i = 0; i < num_vertices; ++i) {
+      Graph::vertex_descriptor v = boost::vertex(i, graph);
 
-      // Compute frontier at this vertex. 
-      State state;
-      WeightedVertexHeap heap;
-      heap.push(WeightedVertex(i, FRONTIER_LOOKAHEAD));
-
-      while(!heap.empty()) {
-        const WeightedVertex &wv = heap.top();
-        
-        if (wv.weight == 0) {
-          state.frontier.push_back(wv.vertex);
-        } else {
-
-          Graph::vertex_descriptor v = boost::vertex(wv.vertex, graph);
-          Graph::adjacency_iterator ai, aend;
-          for (boost::tie(ai, aend) = boost::adjacent_vertices(v, graph); 
-              ai != aend; ++ai) {
-            Point2f loc = graph[*ai].location;
-          }
-
-
-        }
-
-        heap.pop();
+      // Compute the set of possible actions 
+      std::vector<size_t> adjacent_idxs;
+      Graph::adjacency_iterator ai, aend;
+      for (boost::tie(ai, aend) = boost::adjacent_vertices(v, graph); 
+          ai != aend; ++ai) {
+        adjacent_idxs.push_back(indexmap[*ai]);
       }
-      getVisibleVertices(graph, i, i, state.frontier, heap, depth);
 
+      std::vector<Action> actions;
+      actions.push_back(
+
+
+      // For each adjacent idx, crea
     }
   }
 }
