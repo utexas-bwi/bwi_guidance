208d220ce3352c4951d18acb68499fa38234ea69
Piyush Khandelwal <piyushk@gmail.com>
2013-06-20 10:22:09 -0500

    started work on a test case for the mdp solver

--- bwi_exp1/test/test_mdp.cpp
+++ bwi_exp1/test/test_mdp.cpp
@@ -1,12 +1,48 @@
 #include <bwi_exp1/mdp.h>
 #include <topological_mapper/map_loader.h>
 
+using namespace bwi_exp1;
+
 void testStateSpacePopulation(topological_mapper::Graph& graph) {
+
+  size_t num_vertices = boost::num_vertices(graph);
+  State s;
 
   std::vector<bwi_exp1::State> state_space;
   bwi_exp1::populateStateSpace(graph, state_space);
 
+  std::cout << "Size of state cache: " << sizeof(State) * state_space.size() << std::endl; 
 
+  // for each state, get the cached actions (independent of current direction)
+  std::vector<std::vector<Action> > action_space;
+  action_space.resize(num_vertices * 2);
+  long size_action_space = 0;
+  for (size_t graph_id = 0; graph_id < num_vertices; ++graph_id) {
+    s.graph_id = graph_id;
+    for (size_t robots_remaining = 0; robots_remaining <= 1; ++robots_remaining) {
+      s.num_robots_left = robots_remaining;
+      std::vector<Action>& actions =
+        action_space[graph_id * 2 + robots_remaining];
+      getActionsAtState(s, graph, actions);
+      size_action_space += actions.size() * sizeof(Action);
+    }
+  }
+
+  std::cout << "Size of action cache: " << size_action_space << std::endl; 
+
+  // for each state, get the cached next states
+  std::vector<std::vector<size_t> > next_state_space;
+  next_state_space.resize(num_vertices * NUM_DIRECTIONS * (MAX_ROBOTS + 1));
+  long size_next_state_space = 0;
+  for (size_t state_id = 0; state_id < state_space.size(); ++state_id) {
+    getNextStatesAtState(state, statestate_space[state_id], graph, next_state_space[state_id]);
+    size_next_state_space += sizeof(size_t) * next_state_space[state_id].size();
+  }
+
+  std::cout << "Size of next state cache: " << size_next_state_space << 
+    std::endl;
+
+  // 
 
 }
 
