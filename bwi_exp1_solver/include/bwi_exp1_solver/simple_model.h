7bf095934bd5216300a8c36a3662319dbadab4a5
Piyush Khandelwal <piyushk@gmail.com>
2013-07-22 15:34:53 -0500

    ported over the valueiteration class for testing

--- bwi_exp1_solver/include/bwi_exp1_solver/simple_model.h
+++ bwi_exp1_solver/include/bwi_exp1_solver/simple_model.h
@@ -1,7 +1,18 @@
 #ifndef SIMPLE_MODEL_3AJSZE2C
 #define SIMPLE_MODEL_3AJSZE2C
 
+#include <stdint.h>
+
+#include <rl_common/core.hh>
+#include <topological_mapper/graph.h>
+
 namespace bwi_exp1 {
+
+  struct State {
+    size_t graph_id; // ~100
+    size_t direction; // 0 to NUM_DIRECTIONS - 1
+    size_t num_robots_left; // 0 to MAX_ROBOTS
+  };
 
   enum ActionType {
     DO_NOTHING = 0,
@@ -15,6 +26,9 @@ namespace bwi_exp1 {
       ActionType type;
       size_t graph_id; // with PLACE ROBOT, identifies the direction pointed to
   };
+
+  typedef uint32_t state_t;
+  typedef uint32_t action_t;
 
   class PersonModel : public MDPModel {
 
@@ -37,15 +51,12 @@ namespace bwi_exp1 {
 
     private:
 
-      typedef uint32_t state_t;
-      typedef uint32_t action_t;
-
       state_t canonicalizeState(uint32_t graph_id, uint32_t direction, uint32_t robots_remaining) const;
       state_t canonicalizeState(const std::vector<float> &state) const;
       action_t canonicalizeAction(int action) const;
 
       void initializeStateSpace();
-      std::vector<std::vector<State> > state_cache_;
+      std::vector<State> state_cache_;
 
       void initializeActionCache();
       void constructActionsAtState(state_t state, std::vector<Action>& actions);
@@ -53,19 +64,29 @@ namespace bwi_exp1 {
       std::vector<std::vector<Action> > action_cache_;
 
       void initializeNextStateCache();
+      void constructNextStatesAtState(state_t state, 
+          std::vector<state_t>& next_states);
+      std::vector<state_t>& getNextStatesAtState(state_t state);
+      void constructTransitionProbabilities(state_t state_id, action_t action_id, 
+        std::vector<float>& probabilities);
+      std::vector<float>& getTransitionProbabilities(state_t state_id, action_t action_id);
       std::vector<std::vector<state_t> > next_state_cache_;
       std::vector<std::vector<std::vector<float> > > ns_distribution_cache_;
 
+      size_t computeNextDirection(size_t dir, size_t graph_id, size_t next_graph_id);
+      float getAngleFromStates(size_t graph_id, size_t next_graph_id);
+      size_t getDirectionFromAngle(float angle);
+      float getAngleFromDirection(size_t dir);
       uint32_t num_vertices_;
       uint32_t num_directions_;
       uint32_t max_robots_;
       size_t getStateSpaceSize() const;
 
       topological_mapper::Graph graph_;
+      size_t goal_idx_;
 
   };
   
 } /* bwi_exp1 */
-
 
 #endif /* end of include guard: SIMPLE_MODEL_3AJSZE2C */
--- bwi_exp1_solver/src/simple_model.cpp
+++ bwi_exp1_solver/src/simple_model.cpp
@@ -11,20 +11,7 @@ namespace bwi_exp1 {
     initializeNextStateCache();
   }
 
-  /** Update the MDP model with a vector of experiences. */
-  virtual bool updateWithExperiences(std::vector<experience> &instances) = 0;
-
-  /** Update the MDP model with a single experience. */
-  virtual bool updateWithExperience(experience &instance) = 0;
-
-  /** Get the predictions of the MDP model for a given state action */
-  virtual float getStateActionInfo(const std::vector<float> &state, int action, StateActionInfo* retval) = 0;
-
-  /** Get a copy of the MDP Model */
-  virtual PersonModel* getCopy() = 0;
-  virtual ~PersonModel() {};
-
-  state_t PersonModel::canonicalizeState(uint32_t graph_id, uint32_t direction, uint32_t robots_remaining) {
+  state_t PersonModel::canonicalizeState(uint32_t graph_id, uint32_t direction, uint32_t robots_remaining) const {
     return graph_id * num_directions_ * (max_robots_ + 1) + 
       direction * (max_robots_ + 1) +
       robots_remaining;
@@ -36,7 +23,7 @@ namespace bwi_exp1 {
     num_directions_ = 16;
     max_robots_ = 5;
 
-    state_cache_.resize(getStateSpaceSize(num_vertices));
+    state_cache_.resize(getStateSpaceSize());
     for (uint32_t i = 0; i < num_vertices_; ++i) {
       for (uint32_t dir = 0; dir < num_directions_; ++dir) {
         for (uint32_t robots_remaining = 0; robots_remaining <= max_robots_; 
@@ -52,11 +39,11 @@ namespace bwi_exp1 {
 
   void PersonModel::initializeActionCache() {
     action_cache_.resize(num_vertices_ * 2);
-    for (size_t graph_id = 0; graph_id < num_vertices; ++graph_id) {
+    for (size_t graph_id = 0; graph_id < num_vertices_; ++graph_id) {
       for (size_t robots_remaining = 0; robots_remaining <= 1; ++robots_remaining) {
         state_t state = canonicalizeState(graph_id, 0, robots_remaining);
         std::vector<Action>& actions =
-          action_space[graph_id * 2 + robots_remaining];
+          action_cache_[graph_id * 2 + robots_remaining];
         constructActionsAtState(state, actions);
       }
     }
@@ -66,22 +53,24 @@ namespace bwi_exp1 {
     actions.clear();
     actions.push_back(Action(DO_NOTHING,0));
 
-    boost::property_map<Graph, boost::vertex_index_t>::type 
-      indexmap = boost::get(boost::vertex_index, graph);
+    boost::property_map<
+      topological_mapper::Graph, boost::vertex_index_t>::type 
+      indexmap = boost::get(boost::vertex_index, graph_);
 
     State& s = state_cache_[state];
     // Add place robot actions to all adjacent vertices if robots remaining
     if (s.num_robots_left != 0) {
-      Graph::vertex_descriptor v = boost::vertex(s.graph_id, graph);
-      Graph::adjacency_iterator ai, aend;
-      for (boost::tie(ai, aend) = boost::adjacent_vertices(v, graph); 
+      topological_mapper::Graph::vertex_descriptor v = 
+        boost::vertex(s.graph_id, graph_);
+      topological_mapper::Graph::adjacency_iterator ai, aend;
+      for (boost::tie(ai, aend) = boost::adjacent_vertices(v, graph_); 
           ai != aend; ++ai) {
         actions.push_back(Action(PLACE_ROBOT, indexmap[*ai]));
       }
     }
   }
 
-  std::vector<Action>& getActionsAtState(state_t state) {
+  std::vector<Action>& PersonModel::getActionsAtState(state_t state) {
     State& s = state_cache_[state];
     return action_cache_[s.graph_id * 2 + (s.num_robots_left != 0)];
   }
@@ -90,28 +79,32 @@ namespace bwi_exp1 {
     next_state_cache_.resize(getStateSpaceSize());
     for (state_t state = 0; state < getStateSpaceSize(); ++state) {
       std::vector<state_t>& next_states = next_state_cache_[state];
-      constructNextStatesAtState(state, next_state);
+      constructNextStatesAtState(state, next_states);
     }
     ns_distribution_cache_.resize(getStateSpaceSize());
     for (state_t state = 0; state < getStateSpaceSize(); ++state) {
       std::vector<Action>& actions = getActionsAtState(state);
-      
-
+      ns_distribution_cache_[state].resize(actions.size());
+      for (size_t action_id = 0; action_id < actions.size(); ++action_id) {
+        constructTransitionProbabilities(state, action_id, 
+            ns_distribution_cache_[state][action_id]);
+      }
     }
   }
 
-  void PersonModel::constructNextStatesAtState(state_t state, 
+  void PersonModel::constructNextStatesAtState(state_t state_id, 
       std::vector<state_t>& next_states) {
-    State& s = state_cache_[state];
+    State& state = state_cache_[state_id];
     next_states.clear();
-    boost::property_map<Graph, boost::vertex_index_t>::type 
-        indexmap = boost::get(boost::vertex_index, graph);
+    boost::property_map<topological_mapper::Graph, boost::vertex_index_t>::type 
+        indexmap = boost::get(boost::vertex_index, graph_);
 
     // Get all the adjacent vertices
-    Graph::vertex_descriptor v = boost::vertex(state.graph_id, graph);
+    topological_mapper::Graph::vertex_descriptor v = 
+      boost::vertex(state.graph_id, graph_);
     std::vector<size_t> adjacent_idxs;
-    Graph::adjacency_iterator ai, aend;
-    for (boost::tie(ai, aend) = boost::adjacent_vertices(v, graph); 
+    topological_mapper::Graph::adjacency_iterator ai, aend;
+    for (boost::tie(ai, aend) = boost::adjacent_vertices(v, graph_); 
         ai != aend; ++ai) {
       adjacent_idxs.push_back(indexmap[*ai]);
     }
@@ -119,41 +112,43 @@ namespace bwi_exp1 {
     // Now compute all the possible next states
     for (size_t a = 0; a < adjacent_idxs.size(); ++a) {
       uint32_t next_dir = computeNextDirection(state.direction, state.graph_id,
-          adjacent_idxs[a], graph);
+          adjacent_idxs[a]);
 
       // Add next state if we do nothing
       next_states.push_back(
-          constructStateIndex(adjacent_idxs[a], next_dir, state.num_robots_left));
+          canonicalizeState(adjacent_idxs[a], next_dir, state.num_robots_left));
 
       // Add next state if we place a robot
       if (state.num_robots_left != 0) {
         next_states.push_back(
-            constructStateIndex(adjacent_idxs[a], next_dir, state.num_robots_left - 1));
+            canonicalizeState(adjacent_idxs[a], next_dir, state.num_robots_left - 1));
       }
     }
   }
 
-  void PersonModel::
+  std::vector<state_t>& PersonModel::getNextStatesAtState(state_t state) {
+    return next_state_cache_[state];
+  }
 
-  size_t PersonModel::getStateSpaceSize() {
+  size_t PersonModel::getStateSpaceSize() const {
     return num_vertices_ * num_directions_ * (max_robots_ + 1);
   }
 
-  void PersonModel::getTransitionProbabilities(state_t state_id, action_t action_id, 
-      std::vector<state_t>& next_states, std::vector<float>& probabilities) {
+  void PersonModel::constructTransitionProbabilities(state_t state_id, 
+      action_t action_id, std::vector<float>& probabilities) {
 
     // Get all possible next state
-    getNextStatesAtState(state_id, next_states);
+    std::vector<state_t>& next_states = getNextStatesAtState(state_id);
 
     State& state = state_cache_[state_id];
-    Action& action = action_cache_[action_id];
+    Action& action = action_cache_[state_id][action_id];
 
     // In this simple MDP formulation, the action should induce a desired 
     // direction for the person to be walking to.
     float expected_direction, sigma_sq;
     if (action.type == PLACE_ROBOT) {
       expected_direction = 
-        getAngleFromStates(graph, state.graph_id, action.graph_id);
+        getAngleFromStates(state.graph_id, action.graph_id);
       sigma_sq = 0.05;
     } else {
       expected_direction = 
@@ -182,7 +177,7 @@ namespace bwi_exp1 {
       }
 
       float next_state_direction = 
-        getAngleFromStates(graph, state.graph_id, next_state.graph_id);
+        getAngleFromStates(state.graph_id, next_state.graph_id);
 
       // wrap next state direction around expected direction
       while (next_state_direction > expected_direction + M_PI) 
@@ -210,6 +205,34 @@ namespace bwi_exp1 {
     }
     probabilities[last_non_zero_probability] += 1 - normalized_probability_sum;
 
+  }
+
+  size_t PersonModel::computeNextDirection(size_t dir, size_t graph_id, 
+      size_t next_graph_id) {
+    float angle = getAngleFromStates(graph_id, next_graph_id);
+    return getDirectionFromAngle(angle);
+  }
+
+  float PersonModel::getAngleFromStates(size_t graph_id, size_t next_graph_id) {
+
+    topological_mapper::Graph::vertex_descriptor v = 
+      boost::vertex(graph_id, graph_);
+    topological_mapper::Graph::vertex_descriptor next_v =
+      boost::vertex(next_graph_id, graph_);
+
+    return atan2f(graph_[next_v].location.y - graph_[v].location.y,
+        graph_[next_v].location.x - graph_[v].location.x);
+  }
+
+  size_t PersonModel::getDirectionFromAngle(float angle) {
+    angle = angle + M_PI / num_directions_;
+    while (angle < 0) angle += 2 * M_PI;
+    while (angle >= 2 * M_PI) angle -= 2 * M_PI;
+    return (angle * num_directions_) / (2 * M_PI);
+  }
+
+  float PersonModel::getAngleFromDirection(size_t dir) {
+    return ((2 * M_PI) / num_directions_) * dir;
   }
 
 } /* bwi_exp1 */
