cbeb3bc35244037e8694ff0b08305066ebd848ed
Piyush Khandelwal <piyushk@gmail.com>
2013-07-31 15:36:00 -0500

    split up reusable code into module

---
+++ bwi_exp1/src/bwi_exp1/roslaunch.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import subprocess
+
+def start_roslaunch_process(package, binary, args=None, log=None):
+    if args == None:
+        args = {}
+    command_args = ['roslaunch', package, binary]
+    command_args.extend([key + ':=' + value for key, value in args.iteritems()])
+    return (subprocess.Popen(command_args, stdout=log, stderr=subprocess.STDOUT)
+            if log != None else subprocess.Popen(command_args))
+
+def stop_roslaunch_process(process):
+    process.terminate()
+
+
