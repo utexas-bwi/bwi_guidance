b7789a68e087a4ae458c0bdc6eb98456e3be149d
Piyush Khandelwal <piyushk@gmail.com>
2013-08-09 13:08:45 -0500

    improved the interface. now use ActionClient instead of SimpleActionClient for better goal handling. Also enabled different methods as well as target object id

--- orc_interface/src/orc_interface/__init__.py
+++ orc_interface/src/orc_interface/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
 from .comms import MSG, ACK, NACK, ENQ, EOT, send, recv
+from .message import InterfaceMessage, INVOKE, CANCEL, FAILURE, RETURN
+from .move_interface import MoveInterface
 from .server import Server, ClientNotFoundException
 
