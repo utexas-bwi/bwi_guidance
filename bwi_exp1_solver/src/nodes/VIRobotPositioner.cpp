27eb5158c6708c70db12b080fa8eaa086aa366ce
Piyush Khandelwal <piyushk@gmail.com>
2013-08-19 16:14:44 -0500

    added a VI based robot positioner. not sure if generalization to each model is possible.

---
+++ bwi_exp1_solver/src/nodes/VIRobotPositioner.cpp
@@ -0,0 +1,270 @@
+#include<fstream>
+
+#include <bwi_exp1_solver/ValueIteration.h>
+#include <bwi_exp1_solver/person_estimator.h>
+#include <bwi_exp1_solver/person_model.h>
+#include <topological_mapper/map_loader.h>
+#include <bwi_exp1/base_robot_positioner.h>
+
+using namespace bwi_exp1;
+
+class VIRobotPositioner : public BaseRobotPositioner {
+
+  private:
+    boost::shared_ptr<PersonModel> model_;
+    boost::shared_ptr<PersonModel> estimator_;
+    boost::shared_ptr<ValueIteration<state_t, action_t> > vi_;
+
+    std::string policy_file_;
+    unsigned int goal_idx_;
+    double vi_gamma_;
+    unsigned int vi_max_iterations_;
+    bool recompute_policy_;
+
+    size_t prev_graph_id_;
+    size_t num_robots_available_;
+
+  public:
+
+    VIRobotPositioner(boost::shared_ptr<ros::NodeHandle>& nh) :
+        BaseRobotPositioner(nh) {
+
+      ros::NodeHandle private_nh("~");
+      private_nh.getParam("policy_file", policy_file_);
+      private_nh.getParam("goal_idx", goal_idx_);
+      private_nh.param("num_robots_available", num_robots_available, 5);
+      private_nh.param<bool>("recompute_policy", recompute_policy_, false);
+      private_nh.param<double>("vi_gamma", vi_gamma_, 0.98);
+      private_nh.param<int>("vi_max_iterations_", vi_max_iterations_, 1000);
+
+      model_.reset(new PersonModel(graph_, goal_idx));
+      estimator_.reset(new PersonEstimator(model_->getStateSpaceSize(), 0));
+      vi_.reset(new ValueIteration<state_t, action_t>(
+            model_, estimator_, vi_gamma_, vi_max_iterations_));
+
+      bool policyAvailable = false;
+      std::ifstream fin(policy_file_.c_str());
+      if (fin.good()) {
+        policyAvailable = true;
+      }
+      if (policyAvailable && !recompute_policy_) {
+        vi_->loadPolicy(policy_file_);
+        ROS_INFO_STREAM("VIRobotPositioner: Read policy from file: " << 
+            policy_file_);
+      } else {
+        vi.computePolicy();
+        vi.savePolicy(policy_file_);
+        ROS_INFO_STREAM("VIRobotPositioner: Computed policy and saved it " <<
+            "to file: " <<  policy_file_);
+      }
+
+    }
+
+    virtual ~VIRobotPositioner() {}
+
+    virtual void startExperimentInstance(
+        const std::string& instance_name) {
+
+      boost::mutex::scoped_lock lock(robot_modification_mutex_);
+
+      const InstanceRobots& robots_in_instance = 
+        getInstance(instance_name, experiment_robots_);
+
+      topological_mapper::Point2f start_point(robots_in_instance.start_loc.x,
+          robots_in_instance.start_loc.y);
+      start_point = topological_mapper::toGrid(start_point);
+
+      prev_graph_id_ = topological_mapper::getClosestIdOnGraph
+      
+
+    }
+
+    virtual void odometryCallback(const nav_msgs::Odometry::ConstPtr odom) {
+
+      topological_mapper::Point2f person_loc(
+          odom->pose.pose.position.x,
+          odom->pose.pose.position.y);
+
+      boost::mutex::scoped_lock lock(robot_modification_mutex_);
+
+      size_t count = 0;
+      BOOST_FOREACH(const Robot& robot, experiment_robots_.robots) {
+        if (count < assigned_robots_) {
+          topological_mapper::Point2f robot_loc(
+            assigned_robot_locations_[robot.id].position.x,
+            assigned_robot_locations_[robot.id].position.y
+          );
+          float distance = 
+            topological_mapper::getMagnitude(robot_loc - person_loc);
+          if (distance < 3.0) {
+            robot_screen_publisher_.updateImage(robot.id, robot_images_[count]);
+            robot_screen_orientations_[robot.id] = robot_orientations_[count];
+          } else {
+            robot_screen_publisher_.updateImage(robot.id, blank_image_);
+            robot_screen_orientations_[robot.id] = 
+                std::numeric_limits<float>::quiet_NaN(); 
+          }
+        }
+        ++count;
+      }
+
+    }
+
+};
+
+void testValueIteration(topological_mapper::Graph& graph, const std::string& file = "") {
+
+  size_t goal_idx = 33;
+
+  boost::shared_ptr<PersonModel> model(new PersonModel(graph, goal_idx));
+  boost::shared_ptr<PersonEstimator> estimator(
+      new PersonEstimator(model->getStateSpaceSize(), 0));
+  ValueIteration<state_t, action_t> vi(model, estimator, 0.98, 1000);
+
+  bool policyAvailable = false;
+  if (!file.empty()) {
+    std::ifstream my_file(file.c_str());
+    if (my_file.good()) {
+      policyAvailable = true;
+    }
+  }
+  if (policyAvailable) {
+    vi.loadPolicy(file);
+    std::cout << "Read policy from file: " << file << std::endl;
+  } else {
+    vi.computePolicy();
+    vi.savePolicy("policy.txt");
+    std::cout << "Saved policy to file: policy.txt" << std::endl;
+  }
+
+  // Now perform a max walk from start state to goal state
+  while(true) {
+    size_t start_idx, starting_robots, start_direction;
+    std::cout << "Enter start idx: ";
+    std::cin >> start_idx;
+    std::cout << "Enter start direction: ";
+    std::cin >> start_direction;
+    std::cout << "Enter robots remaining: ";
+    std::cin >> starting_robots;
+    state_t current_state_idx = 
+      model->canonicalizeState(start_idx, start_direction, starting_robots);
+    State current_state = 
+      model->resolveState(current_state_idx); 
+    while (current_state.graph_id != goal_idx) {
+      std::cout << "At State (" << current_state.graph_id << ", " << current_state.direction << ", " << current_state.num_robots_left << ")" << std::endl;
+      std::cout << "Value of this state: " << estimator->getValue(current_state_idx) << std::endl;
+      action_t action_idx = vi.getBestAction(current_state_idx);
+      Action action = model->resolveAction(current_state_idx, action_idx);
+      if (action.type == PLACE_ROBOT) {
+        std::cout << "FOUND ROBOT. Robot points towards " << action.graph_id << std::endl;
+      }
+      std::vector<state_t> next_states;
+      std::vector<float> probabilities;
+      std::vector<float> rewards;
+      model->getTransitionDynamics(current_state_idx, action_idx, next_states, 
+          rewards, probabilities);
+
+      for (size_t next_state_counter = 0; next_state_counter < next_states.size(); ++next_state_counter) {
+        state_t next_state_idx = next_states[next_state_counter];
+        State next_state = model->resolveState(next_state_idx);
+        std::cout << "  - #" << next_state_counter << " Leads to State (" << next_state.graph_id << ", " << next_state.direction << ", " << next_state.num_robots_left << ") with probability " << probabilities[next_state_counter] << std::endl;
+      }
+      std::cout << "Choice: ";
+      int choice;
+      std::cin >> choice;
+      current_state_idx = next_states[choice];
+      current_state = model->resolveState(current_state_idx);
+    }
+    std::cout << "VALUE of final state: " << estimator->getValue(current_state_idx) << std::endl;
+  }
+
+}
+
+// void testActionChoices(topological_mapper::Graph& graph) {
+// 
+//   State state;
+//   state.graph_id = 16;
+//   state.num_robots_left = 1;
+//   std::vector<Action> actions;
+//   getActionsAtState(state, graph, actions);
+// 
+//   std::cout << "At State (" << state.graph_id << ", " << state.direction << ", " << state.num_robots_left << ")" << std::endl;
+//   for (size_t action_counter = 0; action_counter < actions.size(); ++action_counter) {
+//     Action& action = actions[action_counter];
+//     std::cout << "  - An action is (" << action.type << ", " << action.graph_id << ")" << std::endl;
+//   }
+// 
+// }
+// 
+// void testNextDirectionComputation(topological_mapper::Graph& graph) {
+// 
+//   size_t direction = 12;
+//   size_t current_state = 78;
+//   size_t next_state = 79;
+//   size_t next_direction;
+// 
+//   next_direction = bwi_exp1::computeNextDirection(direction, current_state, next_state, graph);
+//   std::cout << "(" << current_state << ", " << direction << ") -> " <<
+//       "(" << next_state << ", " << next_direction << ")" << std::endl;
+// 
+//   current_state = next_state;
+//   direction = next_direction;
+//   next_state = 65;
+//   next_direction = bwi_exp1::computeNextDirection(direction, current_state, next_state, graph);
+//   std::cout << "(" << current_state << ", " << direction << ") -> " <<
+//       "(" << next_state << ", " << next_direction << ")" << std::endl;
+// 
+//   current_state = next_state;
+//   direction = next_direction;
+//   next_state = 57;
+//   next_direction = bwi_exp1::computeNextDirection(direction, current_state, next_state, graph);
+//   std::cout << "(" << current_state << ", " << direction << ") -> " <<
+//       "(" << next_state << ", " << next_direction << ")" << std::endl;
+// 
+//   current_state = next_state;
+//   direction = next_direction;
+//   next_state = 55;
+//   next_direction = bwi_exp1::computeNextDirection(direction, current_state, next_state, graph);
+//   std::cout << "(" << current_state << ", " << direction << ") -> " <<
+//       "(" << next_state << ", " << next_direction << ")" << std::endl;
+// 
+//   current_state = next_state;
+//   direction = next_direction;
+//   next_state = 51;
+//   next_direction = bwi_exp1::computeNextDirection(direction, current_state, next_state, graph);
+//   std::cout << "(" << current_state << ", " << direction << ") -> " <<
+//       "(" << next_state << ", " << next_direction << ")" << std::endl;
+// 
+//   current_state = next_state;
+//   direction = next_direction;
+//   next_state = 33;
+//   next_direction = bwi_exp1::computeNextDirection(direction, current_state, next_state, graph);
+//   std::cout << "(" << current_state << ", " << direction << ") -> " <<
+//       "(" << next_state << ", " << next_direction << ")" << std::endl;
+// 
+// }
+
+int main(int argc, char** argv) {
+
+  if (argc < 3) {
+    std::cerr << "USAGE: " << argv[0] 
+        << " <yaml-map-file> <yaml-graph-file> [<saved policy>]" << std::endl;
+    return -1;
+  }
+
+  topological_mapper::MapLoader mapper(argv[1]);
+  topological_mapper::Graph graph;
+  nav_msgs::MapMetaData info;
+  mapper.getMapInfo(info);
+  topological_mapper::readGraphFromFile(argv[2], info, graph);
+
+  //testNextDirectionComputation(graph);
+  //testActionChoices(graph);
+  if (argc >= 4) {
+    testValueIteration(graph, std::string(argv[3]));
+  } else {
+    testValueIteration(graph);
+  }
+
+  return 0;
+}
