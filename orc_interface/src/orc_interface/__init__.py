d196bbf88b74a1d6910e7e66f859983656d69f65
piyushk <piyushk@gmail.com>
2013-12-24 13:57:53 +0530

    moved orc_interface to bwi_orchestration

--- orc_interface/src/orc_interface/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from .comms import MSG, ACK, NACK, ENQ, EOT, send, recv
-from .message import InterfaceMessage, INVOKE, CANCEL, FAILURE, RETURN
-from .move_interface import MoveInterface
-from .list_interface import ListInterface
-from .location_interface import LocationInterface
-from .server import Server, ClientNotFoundException
-
