c997e23f6f7c4aadbc8425515c6a36b2ec2dc49f
Piyush Khandelwal <piyushk@gmail.com>
2014-01-04 15:46:02 -0600

    minor changes to out file

--- bwi_guidance_solver/scripts/produce_combined_results.py
+++ bwi_guidance_solver/scripts/produce_combined_results.py
@@ -4,23 +4,24 @@ import os
 import sys
 
 data_directory = '/projects/agents1/piyushk/'
-if sys.argc > 1:
+if len(sys.argv) > 1:
     data_directory = sys.argv[1]
     print "Processing directory: " + data_directory
 
 max_results = 1000
-if sys.argc > 2:
+if len(sys.argv) > 2:
     max_results = int(sys.argv[2])
 
 outfile = open("out.txt", "w")
 suffix = '_distance.txt'
 for i in range(max_results):
     cur_input_file = data_directory + str(i) + suffix
+    print "Processing " + cur_input_file
     if os.path.exists(cur_input_file):
         infile = open(cur_input_file,  "r")
         line = infile.readline()
         if line:
-            outfile.write(line)
+            outfile.write(line + '\n')
         else:
             print "File " + cur_input_file + "exists, but no data"
         infile.close()
