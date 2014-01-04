1af6ae8c353093371b83bb768a9787804b4831cc
Piyush Khandelwal <piyushk@gmail.com>
2014-01-04 15:34:08 -0600

    first attempt at producing combined results

--- bwi_guidance_solver/scripts/process_exp1_data.py
+++ bwi_guidance_solver/scripts/process_exp1_data.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+import collections
 import csv
+import json
 import matplotlib.pyplot as plt; plt.rcdefaults()
 from matplotlib.font_manager import FontProperties
 import numpy as np
@@ -9,13 +11,92 @@ import sys
 
 # Method names corresponding to method types
 METHOD_NAMES = ['Heuristic', 'VI', 'UCT']
+
+# Keep the following at different length to produce more distinct combinations
+METHOD_COLORS = ['yellow', 'red', 'aqua', 'green', 'lightgray', 'blue']
+METHOD_HATCH = ['/', '\\', 'x', '*', 'o', 'O', '.']
+
+def mean_standard_error(data):
+    a = 1.0 * np.array(data)
+    n = len(a)
+    m, se = np.mean(a), stats.sem(a)
+    return m, se
+
+def mean_confidence_interval(data, confidence=0.95):
+    a = 1.0 * np.array(data)
+    n = len(a)
+    m, se = np.mean(a), stats.sem(a)
+    h = se * stats.t._ppf((1+confidence)/2., n-1)
+    return m, h
+
+def is_significant(a, b, confidence=0.95):
+    t, p = stats.ttest_ind(a, b)
+    return p < (1.0 - confidence)
+
+def draw_bar_chart(samples, top_level_names, second_level_names=None, 
+                  title=None, xlabel=None, ylabel=None):
+
+    # So, samples can either contain a list of lists. The top level list
+    # contains top level groups, and the second level list contains actual
+    # samples (top_level_grouping_only = true)
+
+    # Alternatively, samples may be a list of lists of lists, with top-level 
+    # groups, second level groups and actual samples. (top_level_grouping_only)
+
+    means = []
+    confs = []
+    second_level_grouping_available = \
+            isinstance(samples[0][0], collections.Sequence)
+    top_level_methods = len(samples)
+
+    if second_level_grouping_available: 
+        second_level_methods = len(samples[0])
+        samples2 = samples
+    else:
+        # Create artificial second level grouping
+        second_level_methods = 1
+        samples2 = [[samples[i]] for i in range(top_level_methods)]
+
+    for i in range(top_level_methods):
+        means.append([])
+        confs.append([])
+
+    for i in range(top_level_methods):
+        for j in range(second_level_methods):
+            m, h = mean_standard_error(samples[i][j])
+            means[i].append(m)
+            confs[i].append(h)
+
+    ind = np.arange(second_level_methods)
+    width = 1.0 / (top_level_methods + 1)
+    fig, ax = plt.subplots()
+    rects = []
+    for i in range(top_level_methods):
+        rect = ax.bar(ind + i*width, means[i], width,
+                      color=METHOD_COLORS[i % len(METHOD_COLORS)], 
+                      hatch=METHOD_HATCH[i % len(METHOD_HATCH)],
+                      yerr=confs[i])
+        rects.append(rect)
+
+    if xlabel:
+        ax.set_ylabel(xlabel)
+    if ylabel:
+        ax.set_ylabel(ylabel)
+    if title:
+        ax.set_title(title)
+
+    if second_level_grouping_available:
+        ax.set_xticks(ind+width)
+        if second_level_names:
+            ax.set_xticklabels(second_level_names)
+
+    ax.legend(rects, top_level_names)
+
+    return fig, ax, rects
+
 MAX_ROBOTS = 5 # Compute this from the first line you encounter
-
-METHOD_COLORS = ['y', 'r', 'lightblue']
-METHOD_HATCH = ['/', '\\', 'x']
-
-"""
-import json
+config_file = sys.argv[1]
+print 'Reading from: ' + config_file
 methods = json.load(config_file)
 num_methods = len(methods['methods'])
 method_names = []
@@ -43,32 +124,9 @@ for method in methods['methods']:
         name += "]"
     method_names.append(name)
 
-"""
-def mean_standard_error(data):
-    a = 1.0 * np.array(data)
-    n = len(a)
-    m, se = np.mean(a), stats.sem(a)
-    return m, se
-
-def mean_confidence_interval(data, confidence=0.95):
-    a = 1.0 * np.array(data)
-    n = len(a)
-    m, se = np.mean(a), stats.sem(a)
-    h = se * stats.t._ppf((1+confidence)/2., n-1)
-    return m, h
-
-def is_significant(a, b, confidence=0.95):
-    t, p = stats.ttest_ind(a, b)
-    return p < (1.0 - confidence)
-
-num_methods = 3 
 samples = []
-means = []
-confs = []
 for i in range(num_methods):
     samples.append([])
-    means.append([])
-    confs.append([])
     for j in range(MAX_ROBOTS):
         samples[i].append([])
 
@@ -77,19 +135,16 @@ print 'Reading from: ' + sys.argv[1]
 with open(sys.argv[1], 'rb') as csvfile:
     content_reader = csv.reader(csvfile, delimiter=',')
     for line in content_reader:
-        if first:
-            num_methods = len(line) / MAX_ROBOTS
-            first = False
         for i in range(num_methods):
             for j in range(MAX_ROBOTS):
                 samples[i][j].append(float(line[i + num_methods*j]))
 
-
-for i in range(num_methods):
-    for j in range(MAX_ROBOTS):
-        m, h = mean_standard_error(samples[i][j])
-        means[i].append(m)
-        confs[i].append(h)
+fig, ax, rects = \
+        draw_bar_chart(samples, method_names, 
+                       ('1 Robot', '2 Robots', '3 Robots', '4 Robots', 
+                        '5 Robots'),
+                       title='Average Normalized Distance',
+                       ylabel='Normalized Distance')
 
 # sigs = []
 #     sig = is_significant(vi[i], hs[i])
@@ -98,20 +153,6 @@ for i in range(num_methods):
 #     else:
 #         print "For " + str(i + 1) + " robots, diff is not significant"
 #     sigs.append(sig)
-
-ind = np.arange(5)
-width = 1.0 / (num_methods + 1)
-fig, ax = plt.subplots()
-rects = []
-for i in range(num_methods):
-    rect = ax.bar(ind + i*width, means[i], width, color=METHOD_COLORS[i], hatch=METHOD_HATCH[i], yerr=confs[i])
-    rects.append(rect)
-ax.set_ylabel('Normalized Distance')
-ax.set_title('Average Normalized Distance - ' + sys.argv[2])
-ax.set_xticks(ind+width)
-ax.set_xticklabels( ('1 Robot', '2 Robots', '3 Robots', '4 Robots', '5 Robots') )
-
-ax.legend(rects, METHOD_NAMES[:len(rects)])
 
 # attach some text labels
 # for r in range(len(rects1)):
@@ -123,7 +164,7 @@ ax.legend(rects, METHOD_NAMES[:len(rects)])
 #             ha='center', va='bottom', fontproperties=font)
 
 plt.axhline(y=1.0, xmin=0, xmax=6, linewidth=1, color="black") 
-plt.axis([0, 5, 0, 6])
+plt.axis([0, 6, 0, 6])
 plt.show()
 
 # fig = plt.gcf()
---
+++ bwi_guidance_solver/scripts/produce_combined_results.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+data_directory = '/projects/agents1/piyushk/'
+if sys.argc > 1:
+    data_directory = sys.argv[1]
+    print "Processing directory: " + data_directory
+
+max_results = 1000
+if sys.argc > 2:
+    max_results = int(sys.argv[2])
+
+outfile = open("out.txt", "w")
+suffix = '_distance.txt'
+for i in range(max_results):
+    cur_input_file = data_directory + str(i) + suffix
+    if os.path.exists(cur_input_file):
+        infile = open(cur_input_file,  "r")
+        line = infile.readline()
+        if line:
+            outfile.write(line)
+        else:
+            print "File " + cur_input_file + "exists, but no data"
+        infile.close()
+
+outfile.close()
