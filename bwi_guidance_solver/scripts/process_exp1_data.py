241a56d5cec07c97063b6f923dce258315f9cfa1
Piyush Khandelwal <piyushk@gmail.com>
2014-01-16 14:37:53 -0600

    fixed scripts to produce improved graphs

--- bwi_guidance_solver/scripts/process_exp1_data.py
+++ bwi_guidance_solver/scripts/process_exp1_data.py
@@ -1,98 +1,14 @@
 #!/usr/bin/env python
 
-import collections
+import bwi_tools.graph as graph
 import csv
 import simplejson as json
 import matplotlib.pyplot as plt; plt.rcdefaults()
 from matplotlib.font_manager import FontProperties
-import numpy as np
-import scipy.stats as stats
 import sys
 
 # Method names corresponding to method types
 METHOD_NAMES = ['Heuristic', 'VI', 'UCT']
-
-# Keep the following at different length to produce more distinct combinations
-METHOD_COLORS = ['yellow', 'red', 'aqua', 'green', 'lightgray', 'blue']
-METHOD_HATCH = ['/', '\\', 'x', '*', 'o', 'O', '.']
-
-def mean_standard_error(data):
-    a = 1.0 * np.array(data)
-    n = len(a)
-    m, se = np.mean(a), stats.sem(a)
-    return m, se
-
-def mean_confidence_interval(data, confidence=0.95):
-    a = 1.0 * np.array(data)
-    n = len(a)
-    m, se = np.mean(a), stats.sem(a)
-    h = se * stats.t._ppf((1+confidence)/2., n-1)
-    return m, h
-
-def is_significant(a, b, confidence=0.95):
-    t, p = stats.ttest_ind(a, b)
-    return p < (1.0 - confidence)
-
-def draw_bar_chart(samples, top_level_names, second_level_names=None, 
-                  title=None, xlabel=None, ylabel=None):
-
-    # So, samples can either contain a list of lists. The top level list
-    # contains top level groups, and the second level list contains actual
-    # samples (top_level_grouping_only = true)
-
-    # Alternatively, samples may be a list of lists of lists, with top-level 
-    # groups, second level groups and actual samples. (top_level_grouping_only)
-
-    means = []
-    confs = []
-    second_level_grouping_available = \
-            isinstance(samples[0][0], collections.Sequence)
-    top_level_methods = len(samples)
-
-    if second_level_grouping_available: 
-        second_level_methods = len(samples[0])
-        samples2 = samples
-    else:
-        # Create artificial second level grouping
-        second_level_methods = 1
-        samples2 = [[samples[i]] for i in range(top_level_methods)]
-
-    for i in range(top_level_methods):
-        means.append([])
-        confs.append([])
-
-    for i in range(top_level_methods):
-        for j in range(second_level_methods):
-            m, h = mean_standard_error(samples[i][j])
-            means[i].append(m)
-            confs[i].append(h)
-
-    ind = np.arange(second_level_methods)
-    width = 1.0 / (top_level_methods + 1)
-    fig, ax = plt.subplots()
-    rects = []
-    for i in range(top_level_methods):
-        rect = ax.bar(ind + i*width, means[i], width,
-                      color=METHOD_COLORS[i % len(METHOD_COLORS)], 
-                      hatch=METHOD_HATCH[i % len(METHOD_HATCH)],
-                      yerr=confs[i])
-        rects.append(rect)
-
-    if xlabel:
-        ax.set_ylabel(xlabel)
-    if ylabel:
-        ax.set_ylabel(ylabel)
-    if title:
-        ax.set_title(title)
-
-    if second_level_grouping_available:
-        ax.set_xticks(ind+0.5-width/2)
-        if second_level_names:
-            ax.set_xticklabels(second_level_names)
-
-    ax.legend(rects, top_level_names)
-
-    return fig, ax, rects
 
 MAX_ROBOTS = 5 # Compute this from the first line you encounter
 config_file_name = sys.argv[1]
@@ -148,34 +64,40 @@ with open(sys.argv[2], 'rb') as csvfile:
             for j in range(MAX_ROBOTS):
                 samples[i][j].append(float(line[i + num_methods*j]))
 
-fig, ax, rects = \
-        draw_bar_chart(samples, method_names, 
+fig, ax, rects, means= \
+        graph.draw_bar_chart(samples, method_names, 
                        ('1 Robot', '2 Robots', '3 Robots', '4 Robots', 
                         '5 Robots'),
                        title='Average Normalized Distance',
                        ylabel='Normalized Distance')
 
-# sigs = []
-#     sig = is_significant(vi[i], hs[i])
-#     if sig:
-#         print "For " + str(i + 1) + " robots, diff is significant" 
-#     else:
-#         print "For " + str(i + 1) + " robots, diff is not significant"
-#     sigs.append(sig)
+sigs = None
+if num_methods == 2:
+    sigs = []
+    for robots in range(MAX_ROBOTS):
+        sig = graph.is_significant(samples[0][robots], samples[1][robots])
+        if sig:
+            print "For " + str(robots + 1) + " robots, diff is significant" 
+        else:
+            print "For " + str(robots + 1) + " robots, diff is not significant"
+        sigs.append(sig)
 
-# attach some text labels
-# for r in range(len(rects1)):
-#     height = max(rects1[r].get_height(), rects2[r].get_height())
-#     font = FontProperties()
-#     if sigs[r]:
-#         font.set_weight('bold')
-#     ax.text(rects1[r].get_x()+width, 1.1*height, '%.3f'%(hs_means[r]/vi_means[r]),
-#             ha='center', va='bottom', fontproperties=font)
+#attach some text labels
+if sigs:
+    for r in range(len(rects[0])):
+        height = max(rects[0][r].get_height(), rects[1][r].get_height())
+        font = FontProperties()
+        if sigs[r]:
+            font.set_weight('bold')
+        ax.text(rects[0][r].get_x()+rects[0][r].get_width(), 1.1*height, 
+                '%.3f'%(means[0][r]/means[1][r]),
+                ha='center', va='bottom', fontproperties=font)
 
 plt.axhline(y=1.0, xmin=0, xmax=6, linewidth=1, color="black") 
-plt.axis([0, 5, 0, 8])
-plt.show()
+plt.axis([0, 5, 0, 7])
 
-# fig = plt.gcf()
-# fig.set_size_inches(6,4)
-# plt.savefig('out.png',bbox_inches='tight',pad_inches=0.1,dpi=100)
+fig = plt.gcf()
+fig.set_size_inches(6,4)
+plt.savefig('out.png',bbox_inches='tight',pad_inches=0.1,dpi=100)
+
+plt.show()
--- bwi_guidance_solver/scripts/process_exp2_results.py
+++ bwi_guidance_solver/scripts/process_exp2_results.py
@@ -1,88 +1,64 @@
 #!/usr/bin/env python
 
+import bwi_tools.graph as graph
 import csv
 import matplotlib.pyplot as plt; plt.rcdefaults()
 from matplotlib.font_manager import FontProperties
-import numpy as np
-import scipy.stats as stats
 import sys
 
-def mean_confidence_interval(data, confidence=0.95):
-    a = 1.0 * np.array(data)
-    n = len(a)
-    m, se = np.mean(a), stats.sem(a)
-    h = se * stats.t._ppf((1+confidence)/2., n-1)
-    return m, h
+MAX_ROBOTS = 5
+num_methods = 2
+method_names = ['Heuristic', 'VI']
 
-def mean_standard_error(data):
-    a = 1.0 * np.array(data)
-    n = len(a)
-    m, se = np.mean(a), stats.sem(a)
-    return m, se
-
-def is_significant(a, b, confidence=0.95):
-    t, p = stats.ttest_ind(a, b)
-    return p < (1.0 - confidence)
-
-hs = [[], [], [], [], []]
-vi = [[], [], [], [], []] 
+shortest_distances = [44.2129, 55.305, 46.8513, 47.5942, 59.942]
+samples = [[[], [], [], [], []], [[], [], [], [], []]]
 
 print 'Reading from: ' + sys.argv[1]
 with open(sys.argv[1], 'rb') as csvfile:
     content_reader = csv.reader(csvfile, delimiter=',')
     for line in content_reader:
         if str(line[0]) == 'heuristic':
-            for i in range(5):
-                hs[i].append(float(line[2 + 2*i]))
+            for i in range(MAX_ROBOTS):
+                print 2 + 2*i
+                samples[0][i].append(float(line[2 + 2*i]) / shortest_distances[i])
         else:
-            for i in range(5):
-                vi[i].append(float(line[2 + 2*i]))
+            for i in range(MAX_ROBOTS):
+                samples[1][i].append(float(line[2 + 2*i]) / shortest_distances[i])
 
-vi_means = []
-vi_conf = []
+fig, ax, rects, means= \
+        graph.draw_bar_chart(samples, method_names, 
+                       ('1 Robot', '2 Robots', '3 Robots', '4 Robots', 
+                        '5 Robots'),
+                       title='Average Normalized Distance',
+                       ylabel='Normalized Distance')
 
-hs_means = []
-hs_conf = []
+sigs = None
+if num_methods == 2:
+    sigs = []
+    for robots in range(MAX_ROBOTS):
+        sig = graph.is_significant(samples[0][robots], samples[1][robots])
+        if sig:
+            print "For " + str(robots + 1) + " robots, diff is significant" 
+        else:
+            print "For " + str(robots + 1) + " robots, diff is not significant"
+        sigs.append(sig)
 
-sigs = []
+#attach some text labels
+if sigs:
+    for r in range(len(rects[0])):
+        height = max(rects[0][r].get_height(), rects[1][r].get_height())
+        font = FontProperties()
+        if sigs[r]:
+            font.set_weight('bold')
+        ax.text(rects[0][r].get_x()+rects[0][r].get_width(), 1.1*height, 
+                '%.3f'%(means[0][r]/means[1][r]),
+                ha='center', va='bottom', fontproperties=font)
 
-for i in range(5):
-    m, h = mean_standard_error(vi[i])
-    vi_means.append(m)
-    vi_conf.append(h)
-    m, h = mean_standard_error(hs[i])
-    hs_means.append(m)
-    hs_conf.append(h)
-    sig = is_significant(vi[i], hs[i])
-    if sig:
-        print "For " + str(i + 1) + " robots, diff is significant" 
-    else:
-        print "For " + str(i + 1) + " robots, diff is not significant"
-    sigs.append(sig)
-
-ind = np.arange(5)
-width = 0.35
-fig, ax = plt.subplots()
-rects1 = ax.bar(ind, hs_means, width, color='y', yerr=hs_conf)
-rects2 = ax.bar(ind+width, vi_means, width, color='r', yerr=vi_conf)
-ax.set_ylabel('Distance in meters')
-ax.set_title('Average Distance - Experiment 2')
-ax.set_xticks(ind+width)
-ax.set_xticklabels( ('1 Robot', '2 Robots', '3 Robots', '4 Robots', '5 Robots') )
-
-ax.legend( (rects1[0], rects2[0]), ('Heuristic', 'VI') )
-
-# attach some text labels
-for r in range(len(rects1)):
-    height = max(rects1[r].get_height(), rects2[r].get_height())
-    font = FontProperties()
-    if sigs[r]:
-        font.set_weight('bold')
-    ax.text(rects1[r].get_x()+width, 1.1*height, '%.3f'%(hs_means[r]/vi_means[r]),
-            ha='center', va='bottom', fontproperties=font)
-
-plt.axis([0, 5, 0, 140])
+plt.axhline(y=1.0, xmin=0, xmax=6, linewidth=1, color="black") 
+plt.axis([0, 5, 0, 3.0])
 
 fig = plt.gcf()
 fig.set_size_inches(6,4)
 plt.savefig('out.png',bbox_inches='tight',pad_inches=0.1,dpi=100)
+
+plt.show()
