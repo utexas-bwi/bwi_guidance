1c8a81a7fe95617f225b7eca36960538370fead3
Piyush Khandelwal <piyushk@gmail.com>
2014-01-30 17:40:45 -0600

    ready for processing condor results

---
+++ bwi_guidance_solver/scripts/produce_iros_results.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+data_directory = '/projects/agents1/piyushk/map3/'
+if len(sys.argv) > 1:
+    data_directory = sys.argv[1]
+    print "Processing directory: " + data_directory
+
+max_results = 1000
+if len(sys.argv) > 2:
+    max_results = int(sys.argv[2])
+
+suffixes = ['time.txt','utility.txt','reward.txt']
+for suffix in suffixes:
+    outfile = open(suffix, "w")
+    for i in range(max_results):
+        cur_input_file = data_directory + str(i) + '_' + suffix
+        if os.path.exists(cur_input_file):
+            print "Processing " + cur_input_file
+            infile = open(cur_input_file,  "r")
+            line = infile.readline()
+            if line:
+                outfile.write(line)
+            else:
+                print "File " + cur_input_file + "exists, but no data"
+                infile.close()
+
+    outfile.close()
